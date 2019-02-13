788837185de18ab33e1d4f9517c8387f043db4d6
wagachari <sarafinagachari@gmail.com>
2019-02-13 19:06:46 +0300

    message modified

--- application/modules/friends/controllers/Messages.php
+++ application/modules/friends/controllers/Messages.php
@@ -5,31 +5,26 @@ class Messages extends MX_Controller
         parent:: __construct();
         $this->load->model("messages_model");
         $this->load->model("site/site_model");
-        $this->load->view("site/layouts/layout");
+       
       
      }
 
     public function index()
     {
-        // $this->load->library('pagination');
-        // $this->load->library('table');
-        // $this->db->select('friend_name, friend_age,friend_gender,friend_hobby');
-        // $data['base_url']='http://localhost/ecommerce/friends/hello/index';
-        // $data['total_rows']=$this->db->get('friend')->num_rows();
-        // $data['per_page']=10;
-        // $data['num links']=15;
-        // $data['records']=$this->db->select('friend_name,friend_age,friend_gender,friend_hobby')->get('friend',$data['per_page'],$this->uri->segment(3));
-        // $this->pagination->initialize($data);
-        $data = array(
-            "all_messages" => $this->messages_model->get_message_info(),
+        
+               
+
+        $v_data = array("all_messages" => $this->messages_model->get_message_info(),
         );
-        // $this->load->view("site/layouts/includes/navigation");
-        // $this->load->view("site/layouts/includes/header");
+                
+
+        $data = array(
+            "title" => "ecommerce",
+            "content" =>$this->load->view("friends/all_messages", $v_data, TRUE)
+        );
+        $this->load->view("site/layouts/layout", $data);
+
        
-        
-        $this->load->view("friends/all_messages", $data);
-        // $this->load->view("friends/sidebar");
-        
         
     }
 // public function welcome($friend_id)
